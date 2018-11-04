b1132ed3fc243b1e3b6cf21289e430511f33ddcb
Alireza2018 <ashoaei538@gmail.com>
2018-11-04 18:18:30 +0100

    redirect to gift cards

--- src/Login/atm-screen.js
+++ src/Login/atm-screen.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
 
 import { pinCodeChanged } from '../actions/user'
@@ -6,23 +7,60 @@ import { pinCodeChanged } from '../actions/user'
 
 class Screen extends Component {
 
+  constructor(props) {
+    super(props)
+    this.state = {
+      isLoggedIn : false,
+      redirectToGiftCards : false
+    }
+  }
+
+
+  componentDidMount = () => {
+    if(Object.keys(this.props.user.user).length !== 0) {
+      this.setState({ isLoggedIn : true })
+    }
+  }
+
   handleChange = (e) => {
     this.props.pinCodeChanged(e.target.value)
   }
 
+  goToGiftCards = () => {
+    this.setState({ redirectToGiftCards : true })
+  }
+
   render() {
+
+    if(this.state.redirectToGiftCards) {
+      return <Redirect push to='/gift-cards' />
+    }
+
     return(
       <div className="atm-screen">
         <div className="atm-screen-content">
-          <div className="atm-screen-text">Enter Your Pin Code</div>
-          <div className="w-form">
-            <form id="email-form" name="email-form" data-name="Email Form" className="atm-pincode-form">
-              <input type="password" className="atm-pincode-input w-input" maxLength="256" name="pincode" data-name="pincode" id="pincode"
-                value={ this.props.user.pinCode }
-                onChange={this.handleChange}/>
-            </form>
+            {
+              (Object.keys(this.props.user.user).length == 0) ?
+                <div>
+                  <div className="atm-screen-text">Enter Your Pin Code</div>
+                  <div className="w-form">
+                    <form id="email-form" name="email-form" data-name="Email Form" className="atm-pincode-form">
+                      <input type="password" className="atm-pincode-input w-input" maxLength="256" name="pincode" data-name="pincode" id="pincode"
+                        value={ this.props.user.pinCode }
+                        onChange={this.handleChange}/>
+                    </form>
+                  </div>
+                </div>
+              :
+                <div className="w-form">
+                  <a onClick={this.goToGiftCards}>Select a gift card</a>
+                  <a>Change Personal  Information</a>
+                </div>
+            }
 
-          </div>
+
+
+
         </div>
       </div>
     )
